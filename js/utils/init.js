a2e2219bf16ec98353e9d41a9ca3ddf0d7524401
Rafael Augusto da Silva Ribeiro <rafael@exla.com.br>
2014-11-17 12:30:20 -0200

    0.0.78

--- js/utils/init.js
+++ js/utils/init.js
@@ -47,7 +47,7 @@ function LoginFacebookParse(){
                                         parent.location='unauthorized.html';
                                     }else{
                                         localStorage.setItem("User", JSON.stringify(UsuarioContract.setUsuarioFront(usuario)));
-                                        parent.location='index.htm';
+                                        parent.location='views/htmls/masterpage.html';
                                     }
                                 },
                                 error: function(res, error){
@@ -77,7 +77,7 @@ function LoginFacebookParse(){
                     parent.location='unauthorized.html';
                 }else{
                     localStorage.setItem("User", JSON.stringify(UsuarioContract.setUsuarioFront(usuario)));
-                    parent.location='index.htm';
+                    parent.location='views/htmls/masterpage.html';
                 }
             },
             error: function(res, error) {
